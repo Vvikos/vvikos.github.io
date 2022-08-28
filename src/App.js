37e72a3a4c731bc89f59a097dceb592bc711b5af
Victorv <v.vogt@etu.unistra.fr>
2022-08-28 22:26:58 +0200

    :art: changed font and added bottom animation

--- src/App.js
+++ src/App.js
@@ -62,6 +62,17 @@ export default function App() {
 					<SkillsScreen mainRef={skillsRef}/>
 					<WorkScreen mainRef={workRef}/>
 				</div>
+				<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+					<defs>
+						<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+					</defs>
+					<g>
+						<use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,153,102,0.8)" />
+						<use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,153,102,0.6)" />
+						<use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,153,102,0.4)" />
+						<use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(255,153,102,0.3)" />
+					</g>
+				</svg>
 			</ParallaxProvider>
 		</main>
 	);
