bab04778f26356d65acdd748585e6a20d20bbd63
Victorv <v.vogt@etu.unistra.fr>
2022-09-18 22:47:58 +0200

    :art: added Loading page

---
+++ src/components/loader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Loader() {
+    
+    return (
+        <div class="fullscreen">
+            <div class="wrapper">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="shadow"></div>
+                <div class="shadow"></div>
+                <div class="shadow"></div>
+                <span>Loading</span>
+            </div>  
+        </div>
+    );
+  }
